ef15638a5c6d96fee5bf17b2dc4969994650949d
Sven Marnach <sven@marnach.net>
2017-08-02 19:44:23 +0200

    Move badly named app to zoho_reports.core.

--- zoho_reports/settings/base.py
+++ zoho_reports/settings/base.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'static_pages',
+    'zoho_reports.core',
 ]
 
 MIDDLEWARE = [

b695ca1b1ae9874cdcb430ebd05453bef0c57369
Sven Marnach <sven@marnach.net>
2017-08-10 13:41:49 +0200

    Fix email address list parsing and add tests.

--- zoho_reports/core/models.py
+++ zoho_reports/core/models.py
@@ -53,11 +53,11 @@ class Page(models.Model):
     @property
     def allowed_emails(self):
         "The allowed email addresses as a list"
-        return [email.strip() for email in self._allowed_emails.split()]
+        return [email.strip().lower() for email in self._allowed_emails.split(",")]
 
     def has_access(self, user):
         """Return a bool indicating whether the given Django user has access to the page."""
         return user is not None and (
             user.is_staff or
-            user.email in self.allowed_emails
+            user.email.lower() in self.allowed_emails
         )

4fb926644281652ddc14dd65202d275a25415616
Agrendalath <piotr@surowiec.it>
2020-10-07 23:32:00 +0200

    [BB-2909] Remove redundant `print`

--- zoho_reports/core/backends.py
+++ zoho_reports/core/backends.py
@@ -1,5 +1,4 @@
 """Custom edX OAuth2 backend."""
-
 from auth_backends.backends import EdXOAuth2
 from django.conf import settings
 
@@ -14,7 +13,6 @@ class CustomHeaderEdXOAuth2(EdXOAuth2):
         `SOCIAL_AUTH_EDX_OAUTH2_AUTHORIZATION_HEADER` in settings.
         """
         headers = super().auth_headers()
-        print(headers)
         if (auth_header := settings.SOCIAL_AUTH_EDX_OAUTH2_AUTHORIZATION_HEADER) is not None:
             headers['Authorization'] = auth_header
 
